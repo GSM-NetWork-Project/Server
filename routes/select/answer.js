f256f92545d5cb35587305b53c758702f6084466
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2020-11-26 17:55:21 +0900

    fix: get_answer 요청 시 답변 조회가 아닌 답변 삽입을 시도하던 문제 수정

--- routes/select/answer.js
+++ routes/select/answer.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-    await response.Insert(req, res, 'answer');
+    await response.Select(req, res, 'answer');
 });
 
 module.exports = router;
